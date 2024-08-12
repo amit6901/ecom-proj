cd8b3fb4eb25a8f2c4d2103779460956ddf2e587
amit6901 <amit.kandalkar06@gmail.com>
2024-08-12 21:47:26 +0530

    Added ResponseEntity and getProductById

--- ecom-proj/src/main/java/com/project/ecom_proj/controller/ProductController.java
+++ ecom-proj/src/main/java/com/project/ecom_proj/controller/ProductController.java
@@ -3,10 +3,9 @@ package com.project.ecom_proj.controller;
 import com.project.ecom_proj.model.Product;
 import com.project.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,14 +17,21 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/")
-    public String greet(){
-        return "Welcome home!!!";
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
-    @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+
+        Product product = service.getProductById(id);
+
+        if(product != null)
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 
 }
